2ade3b9c7a2eb3497045f5c0d668aa9f4a9f9ff2
hapo-tuannd <tuannd@haposoft.com>
2021-08-29 17:17:49 +0700

    fake author and import post success

--- app/Converters/GhostJsonConverter.php
+++ app/Converters/GhostJsonConverter.php
@@ -15,6 +15,9 @@ class GhostJsonConverter
             // Read File
             $jsonString = file_get_contents(base_path(self::INPUT_FILE));
             $db = json_decode($jsonString, true);
+            // update version
+            $db['db'][0]['meta']['version'] = "4.12.1";
+
             $data = $db['db'][0]['data'];
 
             // TODO: Update then import test in ghost
@@ -23,8 +26,14 @@ class GhostJsonConverter
             $data = $this->removeUnnecessaryNodes($data);
 
             // Step 2: Convert user
-            for ($i=0; $i < count($data['users']) ; $i++) {
-                $data['users'][$i] = $this->convertUser($data['users'][$i]);
+            // for ($i = 0; $i < count($data['users']); $i++) {
+            //     $data['users'][$i] = $this->convertUser($data['users'][$i]);
+            // }
+            // Step 3: Convert post
+            for ($i = 0; $i < count($data['posts']); $i++) {
+                $postResult = $this->convertPost($data['posts'][$i]);
+                $data['posts'][$i] = $postResult['post'];
+                $data['posts_authors'][] = $postResult['post_author'];
             }
 
             // Affter convert, Re-append data back to db
@@ -55,6 +64,15 @@ class GhostJsonConverter
         unset($data['permissions_apps']);
         unset($data['permissions_roles']);
         unset($data['permissions_users']);
+
+        // test
+        unset($data['roles']);
+        unset($data['roles_users']);
+        unset($data['settings']);
+        unset($data['tags']);
+        unset($data['users']);
+        unset($data['posts_tags']);
+
         return $data;
     }
 
@@ -70,8 +88,6 @@ class GhostJsonConverter
         $user = $this->changekey($user, 'cover', 'cover_img');
         // image -> profile_image
         $user = $this->changekey($user, 'image', 'profile_image');
-        // uuid -> id
-        $user = $this->changekey($user, 'uuid', 'id');
         // last_login -> last_seen
         $user = $this->changekey($user, 'last_login', 'last_seen');
         // language -> locale
@@ -80,6 +96,53 @@ class GhostJsonConverter
         unset($user['created_by']);
         unset($user['updated_by']);
         return $user;
+    }
+
+    public function convertPost(array $post): array
+    {
+        // image -> feature_image
+        $post = $this->changekey($post, 'image', 'feature_image');
+        // markdown -> plaintext
+        $post = $this->changekey($post, 'markdown', 'plaintext');
+        // language -> locale
+        $post = $this->changekey($post, 'language', 'locale');
+
+        // set default author id (test)
+        $post['author_id'] = '1';
+        $post['id'] = uniqid();
+        $post['comment_id'] =  $post['id'];
+
+        $post['canonical_url'] = null;
+        $post['codeinjection_foot'] = null;
+        $post['codeinjection_head'] = null;
+
+        $post['custom_excerpt'] = null;
+        $post['custom_template'] = null;
+        $post['email_recipient_filter'] = "none";
+
+        $post['type'] = "post";
+
+        // remove: created_by, updated_by ...
+        unset($post['created_by']);
+        unset($post['updated_by']);
+        unset($post['amp']);
+        unset($post['meta_description']);
+        unset($post['meta_title']);
+        unset($post['page']);
+        unset($post['published_by']);
+
+        // Fake post author
+        $post_author = [
+            'id' => uniqid(),
+            'post_id' => $post['id'],
+            'author_id' => '1',
+            'sort_order' => 0
+        ];
+
+        return [
+            'post' => $post,
+            'post_author' => $post_author
+        ];
     }
 
     /**
